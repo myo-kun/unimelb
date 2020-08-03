1b9430990774dbc156f9e6af2f780a8ff43657d7
myo-kun <kai.stevensnoguchi@gmail.com>
2020-08-03 09:49:32 +0900

    Add ConstantsVariablesExample

---
+++ OOSD/ConstantsVariablesExample.java
@@ -0,0 +1,13 @@
+public class ConstantsVariablesExample {
+    public static void main(String[] args) {
+        // constant
+        final double PI = 3.1428;
+        // local variable
+        float radius =5.5F;
+        double area;
+        boolean x = true;
+        area = PI * radius * radius;
+        System.out.println("Circle Radius = " + radius + " Area = " + area);
+        System.out.println("Boolean Value = " + x);
+    }
+}
