f6294b05f6face8a889c1289025fa35ae82c0977
myo-kun <kai.stevensnoguchi@gmail.com>
2020-08-13 09:28:56 +0900

    Edit Circle class

--- OOSD/week2/Circle.java
+++ OOSD/week2/Circle.java
@@ -1,8 +1,23 @@
 public class Circle {
-    public double centreX;
-    public double centreY;
-    public double radius;
-
+    public double centreX, centreY, radius;
+    public double getCenterX() {
+        return centreX;
+    }
+    public void setCentreX(double centreX) {
+        this.centreX = centreX;
+    }
+    public double getCentreY() {
+        return centreY;
+    }
+    public void setCentreY(double centreY) {
+        this.centreY = centreY;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
     public double computeCircumference() {
         double circum = 2 * Math.PI * radius;
         return circum;
--- OOSD/week2/CircleTest.java
+++ OOSD/week2/CircleTest.java
@@ -1,13 +1,13 @@
 public class CircleTest {
     public static void main(String[] args) {
         Circle aCircle = new Circle();
-        aCircle.centreX = 10.0;
-        aCircle.centreY = 20.0;
-        aCircle.radius = 5.0;
-        System.out.println("Radius = " + aCircle.radius);
+        aCircle.setCentreX(10.0);
+        aCircle.setCentreY(20.0);
+        aCircle.setRadius(5.0);
+        System.out.println("Radius = " + aCircle.getRadius());
         System.out.println("Circum = " + aCircle.computeCircumference());
         System.out.println("Area = " + aCircle.computeArea());
         aCircle.resize(2.0);
-        System.out.println("Radius = " + aCircle.radius);
+        System.out.println("Radius = " + aCircle.getRadius());
     }
 }
